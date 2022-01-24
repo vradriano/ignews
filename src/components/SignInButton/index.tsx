aad6d20a03975ee76611a67c4f876efbbb2bc0b9
Vitor Adriano <vradriano@hotmail.com>
2022-01-23 22:23:59 -0300

    connection with database FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from 'react-icons/fi'
 import { signIn, signOut, useSession } from 'next-auth/react'
@@ -6,8 +5,6 @@ import styles from './styles.module.scss'
 
 export function SignInButton() {
   const { data: session } = useSession()
-
-  console.log(session)
 
   return session ? (
     <button
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,18 @@
-import NextAuth from "next-auth"
+import { query as q } from 'faunadb'
+
+import NextAuth, { Account, Profile, User } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+    verificationRequest?: boolean;
+  };
+}
 
 export default NextAuth({
   providers: [
@@ -13,4 +26,28 @@ export default NextAuth({
       }
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user
+
+      console.log(user)
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+
+    }
+  }
 })
